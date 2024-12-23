3aa47286bc314ec5e3716985ae047b39f4451310
wuPiotrek <wupiotrek@gmail.com>
2024-12-23 23:02:54 +0100

    app splitted to parts (pane controllers and fxml files for all of)

--- src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -1,61 +1,16 @@
 package pl.javastart.mp3player.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 
 public class MainController {
-
     @FXML
-    private MenuItem fileMenuItem;
-
+    private ContentPaneController contentPaneController;
     @FXML
-    private MenuItem dirMenuItem;
-
+    private ControlPaneController controlPaneController;
     @FXML
-    private MenuItem closeMenuItem;
-
-    @FXML
-    private MenuItem aboutMenuItem;
-
-    @FXML
-    private TableView<?> contentTable;
-
-    @FXML
-    private Button previousButton;
-
-    @FXML
-    private ToggleButton playButton;
-
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private Slider volumeSlider;
-
-    @FXML
-    private Slider progressSlider;
+    private MenuPaneController menuPaneController;
 
     public void initialize() {
-        configureButtons();
-        configureVolume();
-    }
-
-    private void configureVolume() {
-        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
-                System.out.println("Wciśnięto przycisk na suwaku głośności")
-        );
-    }
-
-    private void configureButtons() {
-        previousButton.setOnAction(event -> System.out.println("Poprzednia piosenka"));
-        nextButton.setOnAction(x -> System.out.println("Następna piosenka"));
-        playButton.setOnAction(event -> {
-            if(playButton.isSelected()) {
-                System.out.println("Play");
-            } else {
-                System.out.println("Stop");
-            }
-        });
+        System.out.println("Main controller created");
     }
 }
