e4a1fe5dee16cc53b650bb5f04839234716d92e7
wuPiotrek <wupiotrek@gmail.com>
2024-12-23 19:44:26 +0100

    controller buttons core functions

--- src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -1,42 +1,61 @@
 package pl.javastart.mp3player.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
-
-    @FXML
-    private MenuItem aboutMenuItem;
-
-    @FXML
-    private MenuItem closeMenuItem;
-
-    @FXML
-    private TableView<?> contentTable;
 
     @FXML
     private MenuItem fileMenuItem;
 
     @FXML
-    private MenuItem firMenuItem;
+    private MenuItem dirMenuItem;
 
     @FXML
-    private Button nextButton;
+    private MenuItem closeMenuItem;
 
     @FXML
-    private ToggleButton playButton;
+    private MenuItem aboutMenuItem;
+
+    @FXML
+    private TableView<?> contentTable;
 
     @FXML
     private Button previousButton;
 
     @FXML
-    private Slider progressSlider;
+    private ToggleButton playButton;
+
+    @FXML
+    private Button nextButton;
 
     @FXML
     private Slider volumeSlider;
 
+    @FXML
+    private Slider progressSlider;
+
+    public void initialize() {
+        configureButtons();
+        configureVolume();
+    }
+
+    private void configureVolume() {
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
+                System.out.println("Wciśnięto przycisk na suwaku głośności")
+        );
+    }
+
+    private void configureButtons() {
+        previousButton.setOnAction(event -> System.out.println("Poprzednia piosenka"));
+        nextButton.setOnAction(x -> System.out.println("Następna piosenka"));
+        playButton.setOnAction(event -> {
+            if(playButton.isSelected()) {
+                System.out.println("Play");
+            } else {
+                System.out.println("Stop");
+            }
+        });
+    }
 }
